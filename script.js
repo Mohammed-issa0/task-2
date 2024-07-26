45980ecf42e237c5bd7195ccb6ab6dc54715ea74
Mohammed-issa0 <mohammed.issaopte@gmail.com>
2024-07-27 00:05:28 +0300

    lots of things has added

--- script.js
+++ script.js
@@ -1,8 +1,41 @@
 let name= document.querySelector(".name");
 let email= document.querySelector(".email");
 let num= document.querySelector(".number");
-let pass= document.querySelector(".password");
+let password= document.querySelector(".password");
 let btn = document.querySelector(".btn");
+let emailV;
+
+
+
+name.addEventListener('input', function() {
+            if (this.value.length > 15) {
+                this.value = this.value.slice(0, 15);
+            }
+        });
+num.addEventListener('input', function() {
+            if (this.value.length > 10) {
+                this.value = this.value.slice(0, 10);
+            }
+        });
+password.addEventListener('input', function() {
+            if (this.value.length > 10) {
+                this.value = this.value.slice(0, 10);
+            }
+        });
+
+function validateEmails(emails) {
+            const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            const emailArray = emails.split(',');
+            for (let email of emailArray) {
+                email = email.trim();
+                if (!re.test(email)) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+
 
 let dataArray=[];
 if(localStorage.dataLog != null){
@@ -16,25 +49,54 @@ btn.onclick=function(){
     console.log(name.value);
     console.log(email.value);
     console.log(num.value);
-    console.log(pass.value);
+    console.log(password.value);
 
+    const emailValue = document.querySelector('.email').value;
+    const emailError = document.getElementById('email-error');
+
+    if (!validateEmails(emailValue)) {
+        emailError.style.display = 'block';
+        event.preventDefault();
+        
+    } else {
+        emailError.style.display = 'none';
+        emailV=email.value;
+    }
+            
     let data={
         name: name.value,
         email: email.value,
         number: num.value,
-        password: pass.value,
+        password: password.value,
     }
+
+    
     dataArray.push(data);
     localStorage.setItem('dataLog', JSON.stringify(dataArray));
     clearData();
+    readOnly();
 }
 
     function clearData(){
         name.value='';
         email.value='';
         num.value='';
-        pass.value='';
+        password.value='';
 }
         
 
+const togglePassword = document.querySelector('#togglePassword');
 
+        togglePassword.addEventListener('click', function () {
+            // Toggle the type attribute
+            const type = password.getAttribute('type') === 'password' ? 'text' : 'password';
+            password.setAttribute('type', type);
+            // Toggle the icon
+            this.classList.toggle('bx-show');
+            this.classList.toggle('bx-hide');
+        });
+
+function readOnly(){
+    email.value=emailV;
+    email.setAttribute('readonly' , true);
+}
